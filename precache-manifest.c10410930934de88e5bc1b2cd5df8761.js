298e675b1f7a22013470ded62eaaa9ce77fdc216
Dominika Dyrcz-Ciesielska <ciesielski.michal@interia.eu>
2020-07-06 18:55:12 +0200

    Updates

---
+++ precache-manifest.c10410930934de88e5bc1b2cd5df8761.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ccda4db747ffd796968f7fc3a4e4f9",
+    "url": "/Page-react-ruter/index.html"
+  },
+  {
+    "revision": "73fcc2e2186db520def3",
+    "url": "/Page-react-ruter/static/css/main.c82f84f2.chunk.css"
+  },
+  {
+    "revision": "af2c87ff4494d5649ff7",
+    "url": "/Page-react-ruter/static/js/2.1c98e12b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Page-react-ruter/static/js/2.1c98e12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fcc2e2186db520def3",
+    "url": "/Page-react-ruter/static/js/main.cf0cd10a.chunk.js"
+  },
+  {
+    "revision": "1da99401effffa8d33ee",
+    "url": "/Page-react-ruter/static/js/runtime-main.e9801f79.js"
+  },
+  {
+    "revision": "60db013aa222bd9193464dc366967316",
+    "url": "/Page-react-ruter/static/media/contact.60db013a.jpg"
+  },
+  {
+    "revision": "db456796996a47d4e93e0d4684e16e50",
+    "url": "/Page-react-ruter/static/media/home1.db456796.jpg"
+  },
+  {
+    "revision": "bc9df596fe0bd1cc9d237d38326a3df0",
+    "url": "/Page-react-ruter/static/media/products.bc9df596.jpg"
+  },
+  {
+    "revision": "2f50d4cc72ea444770f9d18d8ad511ad",
+    "url": "/Page-react-ruter/static/media/register.2f50d4cc.jpg"
+  }
+]);
